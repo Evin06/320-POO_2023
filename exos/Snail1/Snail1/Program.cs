0cdd9ce0306bca6b8b6453852ede4d58abadf238
Evin06 <evinsasi@hotmail.com>
2023-08-30 11:08:57 +0200

    Snail v1

---
+++ exos/Snail1/Snail1/Program.cs
@@ -0,0 +1,30 @@
+﻿//caractéristique  de l'escargot
+// affichage de l'escargot 
+//deplacer l'escargot 
+namespace Snail1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Snail = " _@_ö";
+            string Mort = "____";
+            int snailvie = 50;
+            int x= 0;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.CursorVisible = false;
+
+            while (snailvie>0 )
+            {
+                Thread.Sleep(120);
+                Console.SetCursorPosition(x, 10);
+                x++;
+                snailvie--;
+                Console.Write(Snail);
+            }
+            
+           
+            
+        }
+    }
+}
