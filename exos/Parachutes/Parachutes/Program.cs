01b1c8beb29e4f8e9d93153b50715c2dbd2f5699
Evin06 <evinsasi@hotmail.com>
2023-09-06 08:55:57 +0200

    feat : config.cs

---
+++ exos/Parachutes/Parachutes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Parachutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+          
+        }
+   
+    }
+}
