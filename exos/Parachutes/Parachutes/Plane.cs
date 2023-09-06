6b8737b703dcbb593257545a2efbc598c52a0a24
Evin06 <evinsasi@hotmail.com>
2023-09-06 08:56:31 +0200

    feat : Plane.cs

---
+++ exos/Parachutes/Parachutes/Plane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parachutes
+{
+    internal class Plane
+    {
+
+        private string[] view =
+        {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+    }
+}
